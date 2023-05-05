6fb6f266d1b500a486ee171318524f0e216149c4
Bassant Medhat <85830264+bassantmedhat@users.noreply.github.com>
2023-05-05 15:00:27 +0300

    Add files via upload

---
+++ lab4/Release/src/main.d
@@ -0,0 +1,8 @@
+src/main.o: ../src/main.c D:\embeded\labs\LAB5\lab5\Gpio/Gpio.h \
+ D:\embeded\labs\LAB5\lab5\Lib/Std_Types.h \
+ D:\embeded\labs\LAB5\lab5\Rcc/Rcc.h \
+ D:\embeded\labs\LAB5\lab5\Keypad/keypad.h
+D:\embeded\labs\LAB5\lab5\Gpio/Gpio.h:
+D:\embeded\labs\LAB5\lab5\Lib/Std_Types.h:
+D:\embeded\labs\LAB5\lab5\Rcc/Rcc.h:
+D:\embeded\labs\LAB5\lab5\Keypad/keypad.h:
